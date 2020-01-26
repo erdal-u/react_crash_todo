59423267156956f4fa6535af527bee52c2df268a
erdal-u <48831628+erdal-u@users.noreply.github.com>
2020-01-26 12:42:16 +0100

    Updates

---
+++ precache-manifest.37431cc860fc399d44e6b89029521b8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db224fb502004d03a450fedba660d5cd",
+    "url": "/react_crash_todo/index.html"
+  },
+  {
+    "revision": "b95c1a690e742ab9eb17",
+    "url": "/react_crash_todo/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "96732acb8f675bcd8685",
+    "url": "/react_crash_todo/static/js/2.c71a8602.chunk.js"
+  },
+  {
+    "revision": "b95c1a690e742ab9eb17",
+    "url": "/react_crash_todo/static/js/main.95e5fa1a.chunk.js"
+  },
+  {
+    "revision": "e0926076c262068ed090",
+    "url": "/react_crash_todo/static/js/runtime-main.e7fa7380.js"
+  }
+]);
